a07ab0bf049d200ed99e259c7a318d95f27b11a4
ansk <adjevinsk@gmail.com>
2024-10-05 11:54:20 +0200

    Modification sur email dans Models User

--- models/User.js
+++ models/User.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const mongUniqValidator = require('mongoose-unique-validator');
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
+  //email: { type: String, required: true, unique: true },
+  email: { type: String, index: true, unique: true, required: true, uniqueCaseInsensitive: true },
   password: { type: String, required: true }
 });
 
